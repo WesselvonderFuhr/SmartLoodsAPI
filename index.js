636617cfa7ba7e53eeda565664628c6efdea412b
WesselvonderFuhr <38494974+WesselvonderFuhr@users.noreply.github.com>
2021-08-31 15:56:51 +0200

    cors

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 
 
 const connectDB = require('./MongoDB/Connection');
